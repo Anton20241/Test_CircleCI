bde08d33789bb36daab9db474f85815a0fa93259
Anton20241 <90354709+Anton20241@users.noreply.github.com>
2022-02-10 16:41:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Hello, Circle CI" << "\n";
+
+int check(int got, int expected){
+    if (got != expected){
+        std::cout << "Ty loh" << std::endl;
+        return 1;
+    }
+    std::cout << "Ty molodets" << std::endl;
     return 0;
+}
+
+int main(){
+    int result = check(2 + 2, 5);
+    return result;
 }
